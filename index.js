ce5658379b2d64ed2afff6c6725279a2ac98c634
diogo <diogo.lima.flooxai@gmail.com>
2025-03-19 10:38:45 -0300

    feat: implementing json with user

--- index.js
+++ index.js
@@ -9,15 +9,19 @@ var admin = document.querySelector("exampleInputAdmin");
 
 
 var fields = document.querySelectorAll('#form-user-create [name]');
+var user = {};
+
 
 fields.forEach(function(field, index){
 
   if(field.name == 'gender'){
     if (field.checked) {
-      console.log("SIM", field)
+      user[field.name] = field.value
     }
   } else {
-    console.log("Nao")
+    user[field.name] = field.value
   }
 
 });
+
+console.log(user)
