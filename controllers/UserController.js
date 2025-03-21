7272873bac17b446d67fad3e85808b798d74eca3
diogo <diogo.lima.flooxai@gmail.com>
2025-03-21 07:10:27 -0300

    feat: add dataset web api and update user and admin statistics

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -102,6 +102,8 @@ class UserController {
   addLine(dataUser) {
     let tr = document.createElement("tr");
 
+    tr.dataset.user = JSON.stringify(dataUser);
+
     tr.innerHTML = `
       <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
@@ -114,5 +116,29 @@ class UserController {
       </td>`;
 
     this.tableEl.appendChild(tr);
+
+    this.updateCount();
+  }
+
+  updateCount(){
+
+    let numberUsers = 0;
+    let numberAdmin = 0;
+
+    [...this.tableEl.children].forEach(tr =>{
+
+      numberUsers++;
+
+
+     let user = JSON.parse(tr.dataset.user)
+
+     if(user._admin) numberAdmin++;
+
+    });
+
+    document.querySelector("#number-users").innerHTML = numberUsers;
+    document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+    
+
   }
 }
